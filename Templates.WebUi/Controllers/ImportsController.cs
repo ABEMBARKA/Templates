15fee5b57af339d0a9ef748b14c1a9e6bb217ac4
Abdelkhalek BEMBARKA <xtravagon@hotmail.com>
2019-05-23 00:27:42 +0200

    Oficial MDB

---
+++ Templates.WebUi/Controllers/ImportsController.cs
@@ -0,0 +1,13 @@
+﻿namespace Templates.WebUi.Controllers
+{
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class ImportsController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            return await Task.Run(() => View());
+        }
+    }
+}
