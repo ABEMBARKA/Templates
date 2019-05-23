55b6393953bcbb4f75fdfc98df59d273e1c24605
Abdelkhalek BEMBARKA <xtravagon@hotmail.com>
2019-05-23 23:44:14 +0200

    main container adaptation

--- Templates.WebUi/Controllers/ImportsController.cs
+++ Templates.WebUi/Controllers/ImportsController.cs
@@ -9,5 +9,10 @@
         {
             return await Task.Run(() => View());
         }
+
+        public async Task<IActionResult> Table()
+        {
+            return await Task.Run(() => View());
+        }
     }
 }
